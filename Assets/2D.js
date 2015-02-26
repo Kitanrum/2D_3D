003d8b461207273f799a5c411f68cbe48fff5830
Kitanrum <krystel.theuvenin@hotmail.com>
2015-02-26 11:47:57 -0500

    GameFiles

---
+++ Assets/2D.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var moveSpeed : float = 5;
+private var death : boolean = false;
+
+function Update () {
+
+	var h : float = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+	var jump : float;
+
+	transform.Translate(h, 0, 0);
+
+		//yield WaitForSeconds(2);
+
+	if(Input.GetButtonDown("Jump")){
+
+		rigidbody.velocity.y = 5;
+
+		//yield WaitForSeconds (2);
+	}
+
+
+
+
+}
+
+function OnTriggerEnter (death : Collider){
+
+		Application.LoadLevel (0);
+}
---
+++ Assets/3D.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var moveSpeed : float = 5;
+var xSensitivity : float = 5;
+var ySensitivity : float = 5;
+
+function Update () {
+
+	var h : float = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+	var d : float = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+	var xRot : float = Input.GetAxis("Mouse X") * xSensitivity;
+	var yRot : float = -Input.GetAxis("Mouse Y") * ySensitivity;
+
+	transform.Translate(h,0,d);
+	transform.Rotate(yRot, xRot, 0);
+
+	if(Input.GetButtonDown("Jump")){
+
+		rigidbody.velocity.y = 5;
+	}
+
+}
+
+function OnTriggerEnter (death : Collider){
+
+	Application.LoadLevel(1);
+}
